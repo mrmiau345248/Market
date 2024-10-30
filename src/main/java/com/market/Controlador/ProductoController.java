d28045af083887134590fe2b97dfaad8ecceb3c9
mrmaiu345248 <sl39718@gmail.com>
2024-10-29 21:30:53 -0500

    vance 0.7.1

--- src/main/java/com/market/Controlador/ProductoController.java
+++ src/main/java/com/market/Controlador/ProductoController.java
@@ -8,7 +8,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/producto")
+@RequestMapping("api/Marketplace/producto")
 public class ProductoController {
     private final ProductoService productoService;
     private final RepoProducto repoProducto ;
@@ -20,12 +20,26 @@ public class ProductoController {
         this.conversionProducto = conversionProducto;
     }
 
-    @PostMapping
+    @PostMapping("/crear")
     public ResponseEntity<ProductoDto> crearProducto(@RequestBody ProductoDto productoDto) {
         ProductoDto nuevoProducto = productoService.crearProducto(productoDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
     }
-
-
-
+    @GetMapping("/traer/{id}")
+    public ResponseEntity<ProductoDto> traerProducto(@PathVariable  int id){
+        ProductoDto producto = productoService.traerProducto(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .header("Content-type","application/json")
+                .body(producto);
+    }
+    @PutMapping("/modificar/{id}")
+    public ResponseEntity<ProductoDto> modificarProducto(@PathVariable int id) {
+        System.out.println("Holaaaaaaaaa" );
+        ProductoDto nuevoProducto = productoService.traerProducto(id);
+        if (nuevoProducto != null) {
+            return ResponseEntity.ok(nuevoProducto); // 200 OK
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // 404 Not Found
+        }
+    }
 }
